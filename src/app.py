da691b4e306863f9679feb3a4f6950de00117698
Spencer Gerlach <spence.gerlach@gmail.com>
2023-03-14 11:19:29 -0600

    all charts working

--- src/app.py
+++ src/app.py
@@ -1,33 +1,124 @@
 from dash import dash, html, dcc, Input, Output
 import dash_bootstrap_components as dbc
 import altair as alt
-from vega_datasets import data
+import pandas as pd
+import geopandas as gpd
 
-cars = data.cars()
+alt.data_transformers.enable('data_server')
 
-def plot_altair(xmax):
-    chart = alt.Chart(cars[cars["Horsepower"] < xmax]).mark_point().encode(
-        x = "Horsepower",
-        y = "Weight_in_lbs"
-    )
-    return chart.to_html()
+# Read data
+df = pd.read_csv('../data/clean/permit_cleaned.csv')
+gdf = gpd.read_file('../data/clean/geo_nbhd_summary_long.geojson')
 
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
+# UI
 app.layout = html.Div([
-        html.H1("Test Dash App"),
+        html.H1("Vancouver Building Permits"),
+        html.Br(),
+        html.Label("X-axis:"),
+        dcc.Dropdown(
+            id='selected_x1',
+            options=[{'label': col, 'value': col} for col in ['PermitElapsedDays', 'ProjectValue']],
+            value='PermitElapsedDays'
+        ),
+        html.Br(),
         html.Iframe(
-            id='scatter',
-            srcDoc=plot_altair(xmax=0),
-            style={'border-width': '0', 'width': '100%', 'height': '400px'}),
-        dcc.Slider(id='xslider', min=0, max=240)])
+            id='bars',
+            style={'border-width': '0', 'width': '100%', 'height': '600px'}),
+        html.Br(),
+        dcc.Dropdown(
+            id='selected_use',
+            options=[{'label': col, 'value': col} for col in ['1FD on Sites w/ More Than One Principal Building', 
+                                                              '1FD w/ Family Suite', 
+                                                              '2FD on Sites w/ Mult Principal Bldg', 
+                                                              'Duplex', 
+                                                              'Duplex w/Secondary Suite', 
+                                                              'Freehold Rowhouse', 
+                                                              'Housekeeping Unit', 
+                                                              'Infill', 
+                                                              'Infill Multiple Dwelling', 
+                                                              'Infill Single Detached House', 
+                                                              'Infill Two-Family Dwelling', 
+                                                              'Laneway House', 
+                                                              'Micro Dwelling', 
+                                                              'Mixed', 
+                                                              'Multiple Conv Dwelling w/ Family Suite', 
+                                                              'Multiple Conv Dwelling w/ Sec Suite', 
+                                                              'Multiple Conversion Dwelling', 
+                                                              'Not Applicable', 
+                                                              'Principal Dwelling Unit w/Lock Off', 
+                                                              'Residential Unit Associated w/ an Artist Studio', 
+                                                              'Residential/Business Unit', 
+                                                              'Rooming House', 
+                                                              'Seniors Supportive/Assisted Housing', 
+                                                              'Single Detached House', 
+                                                              'Single Detached House w/Sec Suite', 
+                                                              'Sleeping Unit', 
+                                                              'Temporary Modular Housing', 
+                                                              'Dwelling Unit', 
+                                                              'Dwelling Unit w/ Other Use', 
+                                                              'Multiple Dwelling', 
+                                                              'Secondary Suite']],
+            value=['Duplex', 'Infill', 'Multiple Dwelling'],
+            multi=True
+        ),
+        html.Br(),
+        html.Iframe(
+            id='lines',
+            style={'border-width': '0', 'width': '100%', 'height': '600px'}),
+        html.Br(),
+        dcc.Dropdown(
+            id='selected_map',
+            options=[{'label': col, 'value': col} for col in ['elapsed_days_avg', 
+                                                              'project_value_avg',
+                                                              'count_permits']],
+            value='count_permits'
+        ),
+        html.Br(),
+        html.Iframe(
+            id='map',
+            style={'border-width': '0', 'width': '100%', 'height': '600px'}),
+        ])
 
+# Server
 @app.callback(
-    Output('scatter', 'srcDoc'),
-    Input('xslider', 'value')
+    Output(component_id='bars', component_property='srcDoc'),
+    Output(component_id='lines', component_property='srcDoc'),
+    Output(component_id='map', component_property='srcDoc'),
+    Input(component_id='selected_x1', component_property='value'),
+    Input(component_id='selected_use', component_property='value'),
+    Input(component_id='selected_map', component_property='value')
 )
-def update_output(xmax):
-    return plot_altair(xmax)
+def plot_altair(x_axis, uses, map_stat):
+    
+    chart = alt.Chart(df).mark_bar().encode(
+        x = alt.X("mean(%s)" % (x_axis), 
+                  title = "Mean of %s" % (x_axis)),
+        y = alt.Y("GeoLocalArea", 
+                  sort = 'x', 
+                  title = "Neighbourhood"),
+        tooltip=alt.Tooltip("mean(%s)" % (x_axis),
+                            format = '~f')
+    ).interactive()
+    
+    df_filtered = df.loc[df['SpecificUseCategory'].isin(uses)]
+    # df_filtered = df[df['SpecificUseCategory'].str.contains('|'.join(uses))]
+    chart2 = alt.Chart(df_filtered).mark_line().encode(
+        x = alt.X("YearMonth"),
+        y = alt.Y("count()"),
+        color = alt.Color("SpecificUseCategory"),
+        tooltip=alt.Tooltip(["SpecificUseCategory", "count()"])
+    ).interactive()
 
+    gdf_filtered = gdf.loc[gdf['stat'] == map_stat]
+    chart3 = alt.Chart(gdf_filtered, title = "Chloropleth Map of Vancouver Neighbourhoods").mark_geoshape().encode(
+        color = alt.Color("value"),
+        tooltip = ["name", "value"]
+    ).project(type="identity", reflectY=True)
+
+    return chart.to_html(), chart2.to_html(), chart3.to_html()
+
+# Run the bish
 if __name__ == '__main__':
     app.run_server(debug=True)
